b0ca79611fabe0c7dab86f708a0ae0854297cb3e
gidoo9933 <oseigideon@gmail.com>
2023-02-16 16:18:47 -0800

    Print to a new line and standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
+#include <stdio.h>
 #include <unistd.h>
 
-/**                                                   
- * main - prints a string and standard error
- * Return: 1                                          
-*/
-int main(void)                                       {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+/**
+ * main - Entry point
+ * Description: " piece of art is useful" - Dora Korpar, 2015-10-19'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
-
