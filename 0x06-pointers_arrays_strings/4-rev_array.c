846ab9131af91448c77e860f5cab95adec89b4e6
gidoo9933 <oseigideon@gmail.com>
2023-03-02 13:50:22 -0800

    Function capitalises all words of a string

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array of integers
+ * @a: array
+ * @n: number of elements of array
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n) /* reverse array of integers */
+
+{
+
+int i;
+
+int t;
+
+
+for (i = 0; i < n--; i++)
+
+{
+
+t = a[i];
+
+a[i] = a[n];
+
+a[n] = t;
+
+}
+
+}
