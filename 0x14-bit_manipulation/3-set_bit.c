cc58c231cfa356f325db8c94bd659c051bb76753
gidoo99333 <gidoo9933@gmail.com>
2023-04-10 11:52:28 -0700

    set bit at a given index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,21 +1,19 @@
 #include "main.h"
 
 /**
-* get_bit - returns the value of a bit at a given index
-* @n: number where bit will be found
+* set_bit - sets the value of a bit at a given index
+* @n: pointer where bit will be found
 * @index: index of the bit whose value is to be returned
 *
 * Return: the value of the bit or -1 if an error occured
 */
 
-int get_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
-	int bitNum;
-
-	if (index > 58)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	bitNum = (n >> index) & 1;
+	*n ^= (1 << index);
 
-	return (bitNum);
+	return (1);
 }
