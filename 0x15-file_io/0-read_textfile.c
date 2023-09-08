b3b87b7e9c2adf57b0f00d2809785aff0b463b4a
gidoo9933 <gidoo9933@gmail.com>
2023-09-08 06:26:28 -0700

    function that reads a textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,41 +3,29 @@
 
 
 /**
-* read_textfile- Read text file print to STDOUT.
-* @filename: text file being read
-* @letters: number of letters to be read
-* Return: actual number of bytes read and printed
-*        0 when function fails or filename is NULL.
+* read_textfile- function that reads a text file and print to STDOUT.
+* @filename: the name of text file being read
+* @letters: number of letters to be read from file
+* Return: the actual number of bytes read and printed
+*        0 otherwise, when function fails or filename is NULL.
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buff;
-	ssize_t fd;
-	int rdBytes, wrtBytes;
+char *buff;
+ssize_t fd;
+int wrtBytes;
+int rdBytes;
 
 
-	if (!filename)
-	return (0);
-
-	buff = malloc(sizeof(char) * letters);
-		if (!buff)
-		return (0);
-
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+buff = malloc(sizeof(char) * letters);
+rdBytes = read(fd, buff, letters);
+wrtBytes = write(STDOUT_FILENO, buff, rdBytes);
 
 
-	rdBytes = read(fd, buff, letters);
-	wrtBytes = write(STDOUT_FILENO, buff, letters);
-
-	if (rdBytes == -1 || rdBytes == wrtBytes || wrtBytes == -1)
-		{
-			free(buff);
-			close(fd);
-			return (0);
-		}
-
-	close(fd);
-	return (wrtBytes);
+free(buff);
+close(fd);
+return (wrtBytes);
 }
